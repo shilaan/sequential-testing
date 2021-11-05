10f8d1f36a689b7cac821e4c986726ddba498233
shilaan <shilaan@stanford.edu>
2021-11-05 05:12:17 +0100

    created figures S1, S2, and S3

--- supplemental/figures/figures_supplemental.R
+++ supplemental/figures/figures_supplemental.R
@@ -1,7 +1,4 @@
 # Supplemental Materials
-# Add figures comparing ISP to Fixed (code for these sims in 'FixedvsISPsims.Rmd')
-# Add Google Drive > Independent Segments Procedure > Simulations > Figure 7 (Bias-Variance in Obtained Effect Sizes using Fixed versus Independent Segments Hypothesis Testing).  Code in 'Figures.Rmd'
-# Add Google Drive > Independent Segments Procedure > Simulations > Figure 6 (Estimated Cohen's d using Fixed versus Independent Segments Hypothesis Testing).  Code in 'Figures Appendix.Rmd'
 
 # Set up ------------------------------------------------------------------
 library(ggplot2)
@@ -10,8 +7,18 @@ library(ggtext)
 library(grid)
 library(gridExtra)
 
+
+# Read data ---------------------------------------------------------------
 zipped_df = gzfile("supplemental/simulations/data_supplemental.csv.gz", "rt")
 df = read.csv(zipped_df, header = T) 
+
+df_summary = df %>% 
+  group_by(d_forpower, power, proc, d_actual) %>%  
+  summarize(mean_ES = mean(ES),
+            mse = mean((ES - d_actual)^2),
+            var = mean((ES - mean(ES))^2)) %>% 
+  mutate(bias = mean_ES - d_actual,
+         bias2 = (mean_ES - d_actual)^2) 
 
 # Figure S1 ----------------------------------------------------------------
 # Obtained Effect Sizes using Fixed versus Independent Segments Hypothesis Testing
@@ -49,27 +56,28 @@ p = lapply(split(df_expected, list(df_expected$power, df_expected$d_actual), dro
 p[[1]] = p[[1]] + #manually set legend in first facet
   annotate("segment", x = -0.2, y = 4.5, xend = -.1, yend = 4.5) +
   annotate("segment", x = -0.2, y = 3.5, xend = -.1, yend = 3.5, linetype = "dashed") +
-  annotate(geom = "text", x = -.08, y = 4.3, label = "ISP", hjust = 0) +
+  annotate(geom = "text", x = -.08, y = 4.5, label = "ISP", hjust = 0) +
   annotate(geom = "text", x = -.08, y = 3.5, label = "Fixed", hjust = 0) 
 
-tiff(file="supplemental/figures/figureS1.tiff",width=2100,height=2700, units = "px", res = 300)
-figS1 <- grid.arrange(
+#tiff(file="supplemental/figures/figureS1.tiff",width=2100,height=2700, units = "px", res = 300)
+quartz(type = "pdf", file = "supplemental/figures/figureS1.pdf", width=9, height = 8)
+grid.arrange(
   grobs = p, 
   ncol = 3, 
   top = textGrob(
     "Obtained Effect Sizes using Fixed versus Independent Segments Hypothesis Testing", 
     gp = gpar(fontsize = 12)
-    ),
+  ),
   bottom = textGrob(
     "Observed Cohen's d", 
     gp = gpar(fontsize = 12)
-    ),
+  ),
   left = textGrob(
     "Density", 
     gp=gpar(fontsize = 12), 
     rot = 90, 
     just = "right")
-  )
+)
 dev.off()
 
 # Figure S2 ----------------------------------------------------------------
@@ -78,34 +86,28 @@ dev.off()
 # Focus is only on power = 0.8
 # Add Google Drive > Independent Segments Procedure > Simulations > Figure 4 (Bias in Obtained Effect Sizes using Fixed versus Independent Segments Hypothesis Testing). Code in 'Figures.Rmd'
 
-df_unexpected <- df %>% 
-  filter(power == 0.8)
-
-df_summary = df_unexpected %>% 
-  group_by(d_forpower, proc, d_actual) %>%  
-  summarize(ES = mean(ES)) %>% 
-  mutate(bias = ES - d_actual)
-
 bplot = function(f) {
   ggplot(data = f, 
          mapping = aes(x = d_actual, 
                        y = bias, 
                        group = proc, 
                        linetype = proc)) +
-    geom_smooth(color = "black", se = F, size = .6, method = "gam", formula = y ~ s(x)) + 
-    #there is *some* bias in the fixed procedure, but it's imperceptibly small, especially when using a smoother  
+    geom_smooth(color = "black", 
+                se = F, 
+                size = .6, 
+                method = "gam", 
+                formula = y ~ s(x, k = 8)) + 
     theme_classic() +
-    #geom_smooth(color = "black", 
-     #           se = F, 
-      #          size = .6, 
-       #         method = "gam", 
-        #        formula = y ~ s(x, k = 11)) +
     scale_x_continuous(breaks = seq(0, 1, 0.2),
                        labels = paste0("\u03B4 = ", seq(0, 1, 0.2))) +
     scale_y_continuous(breaks = c(-max(abs(f$bias)), 0, max(abs(f$bias))),
                        labels = c(round(-max(abs(f$bias)), 2), 0, round(max(abs(f$bias)), 2)),
                        limits = c(-max(abs(f$bias)), max(abs(f$bias)))) +
-    labs(title = paste0("H\u00b2: \u03B4 = ", mean(f$d_forpower), ", 1 - *\u03b2* = 0.8")) +
+    scale_linetype_manual(values=c("dashed", "solid")) + 
+    labs(title = paste0("H\u00b2: \u03B4 = ", 
+                        mean(f$d_forpower), 
+                        ", 1 - *\u03b2* = ", 
+                        mean(f$power))) +
     theme(
       axis.title = element_blank(),
       plot.title = ggtext::element_markdown(hjust = 0.5),
@@ -115,22 +117,20 @@ bplot = function(f) {
       )
 }
 
-bp = lapply(split(df_summary, df_summary$d_forpower), bplot) #create 4 plots, one for each d_forpower
-bp[[1]] = bp[[1]]  + annotate("segment", x = 0.6, y = .02, xend = 0.7, yend = .02, linetype = "dashed") +
-  annotate("segment", x = 0.6, y = .01, xend = 0.7, yend = .01) +
-  annotate(geom = "text", x = .7, y = .02, label = "ISP", hjust = 0) +
-  annotate(geom = "text", x = .7, y = .01, label = "Fixed", hjust = 0) #manually set legend in first plot
+bp = lapply(split(df_summary, list(df_summary$d_forpower, df_summary$power)), bplot) #create 4 plots, one for each d_forpower
+bp[[1]] = bp[[1]]  + 
+  annotate("segment", x = 0.6, y = .05, xend = 0.75, yend = .05) +
+  annotate("segment", x = 0.6, y = .035, xend = 0.75, yend = .035, linetype = "dashed") +
+  annotate(geom = "text", x = .8, y = .05, label = "ISP", hjust = 0) +
+  annotate(geom = "text", x = .8, y = .035, label = "Fixed", hjust = 0) #manually set legend in first plot
 
-tiff(file="supplemental/figures/figureS2.tiff",
-     width=3000,
-     height=3000, 
-     units = "px", 
-     res = 300)
-figS2 <- grid.arrange(
+#tiff(file="supplemental/figures/figureS2.tiff", width=4000, height=3000, units = "px", res = 300)
+quartz(type = "pdf", file = "supplemental/figures/figureS2.pdf", width=13, height = 9)
+grid.arrange(
   grobs = bp, 
-  ncol=2, 
+  ncol = 4, 
   top = textGrob("Bias in Obtained Effect Sizes using Fixed versus Independent Segments Hypothesis Testing", 
-                 gp = gpar(fontsize = 12)),
+             gp = gpar(fontsize = 12)),
   bottom = textGrob("True Effect Size (Cohen's d)", 
                     gp = gpar(fontsize = 12)),
   left = textGrob("Average Estimated Effect Size - True Effect Size", 
@@ -140,26 +140,73 @@ figS2 <- grid.arrange(
 
 dev.off()
 
-quartz(type = 'pdf', file = 'Figure4.pdf', width=15)
-grid.arrange(grobs = bp, ncol=4, top = textGrob("Bias in Obtained Effect Sizes using Fixed versus Independent Segments Hypothesis Testing \n Number of Simulations = 10,000", gp = gpar(fontsize = 12)),
-             bottom = textGrob("True Effect Size (Cohen's d)", gp = gpar(fontsize = 12)),
-             left = textGrob("Average Estimated Effect Size - True Effect Size", gp=gpar(fontsize = 12), rot = 90, just = "center"))
-
-
-
-grid.arrange(grobs = bp, ncol =4, top = textGrob("Bias in Obtained Effect Sizes using Fixed versus Independent Segments Hypothesis Testing \n Number of Simulations = 10,000", gp = gpar(fontsize = 12)),
-             bottom = textGrob("True Effect Size (Cohen's d)", gp = gpar(fontsize = 12)),
-             left = textGrob("Average Significant Effect - True Effect Size", gp=gpar(fontsize = 12), rot = 90, just = "center"))
-
-
-
 # Figure S3 ---------------------------------------------------------------
 # Bias-Variance in Obtained Effect Sizes using Fixed versus Independent Segments Hypothesis Testing
+# Add Google Drive > Independent Segments Procedure > Simulations > Figure 7 (Bias-Variance in Obtained Effect Sizes using Fixed versus Independent Segments Hypothesis Testing).  Code in 'Figures.Rmd'
 
+proc.label = c("Fixed Sample Hypothesis Test", "Independent Segments Procedure")
+names(proc.label) = c("Fixed", "ISP")
+plt = function(f) {
+  D = mean(f$d_forpower) #get separate plot for each d powered for
+  t = paste0(sprintf("H\u00b2: \u03b4 = %0.1f, 1 - *\u03b2* = ", D), mean(f$power))
+  
+  ggplot(data = f, mapping = aes(x = d_actual, y = bias2)) +
+    geom_smooth(method = "gam", formula = y ~ s(x, k = 12), se = F, color = "black") +
+    #geom_smooth(color = "black", se = F, size = .6, method = "gam") + #bias2
+    geom_smooth(mapping = aes(y = mse), se = F, size = .6, method = "gam",
+                linetype = "dashed", color = "red") + #MSE
+    geom_smooth(mapping = aes(y = var), linetype = "dotted", se = F, 
+                size = .6, method = "gam") + #variance
+    scale_x_continuous(breaks = seq(0, 1, 0.2),
+                       labels = paste0("\u03B4 = ", seq(0, 1, 0.2))) +
+    scale_y_continuous(breaks = c(0, max(f$mse)/2,  max(f$mse)),
+                       labels = c(0, round(max(f$mse)/2, 2), round(max(f$mse), 2)),
+                       limits = c(0, max(f$mse))) +
+    theme_bw() +
+    labs(title = t) +
+    facet_wrap(vars(proc),labeller = labeller(proc = proc.label)) +
+    theme(axis.title = element_blank(),
+          strip.background =element_rect(fill="white"),
+          plot.title = ggtext::element_markdown(size = 12, hjust = 0.5),
+          axis.text.x = ggtext::element_markdown(size = 6, angle = 45, hjust = 1))
+}
 
+bvp = lapply(split(df_summary, list(df_summary$d_forpower, df_summary$power)), plt) #create 12 plots, one for each combination of d_forpower and power
+
+facet1 = data.frame(proc = "Fixed", 
+                    label1 = "Mean Squared Error",
+                    label2 = "Variance",
+                    label3 = "Squared Bias",
+                    x1 = 0.1, y1 = .025, y2 = .020, y3 = .015, x0 = 0)
+
+bvp[[1]] = bvp[[1]] +
+  geom_text(data = facet1, aes(x = x1, y = y1, label = label1, hjust = 0), size = 3,
+            color = "red") +
+  geom_text(data = facet1, aes(x = x1, y = y2, label = label2, hjust = 0), size = 3,
+            color = "blue") +
+  geom_text(data = facet1, aes(x = x1, y = y3, label = label3, hjust = 0), size = 3) +
+  geom_segment(data = facet1, aes(x = x0, xend = x1, y = y1, yend = y1),
+               linetype = "dashed", color = "red") +
+  geom_segment(data = facet1, aes(x = x0, xend = x1, y = y2, yend = y2),
+               linetype = "dotted", color = "blue") +
+  geom_segment(data = facet1, aes(x = x0, xend = x1, y = y3, yend = y3))
+
+quartz(type = 'pdf', file = 'supplemental/figures/figureS3.pdf', width=20, height = 10.5)
+grid.arrange(
+  grobs = bvp, 
+  ncol = 4, 
+  top = textGrob(
+    "Bias-Variance in Obtained Effect Sizes using Fixed versus Independent Segments Hypothesis Testing",
+    gp = gpar(fontsize = 12)
+    ), 
+  bottom = textGrob(
+    "True Effect Size (Cohen's \u03B4)", 
+    gp = gpar(fontsize = 12)
+    )
+  )
+dev.off()
 # Figure S4 ---------------------------------------------------------------
 # Exaggeration of Significant Results using Fixed versus Independent Segments Hypothesis Testing
-
-
+# Add Google Drive > Independent Segments Procedure > Simulations > Figure 6 (Estimated Cohen's d using Fixed versus Independent Segments Hypothesis Testing).  Code in 'Figures Appendix.Rmd'
 
 
